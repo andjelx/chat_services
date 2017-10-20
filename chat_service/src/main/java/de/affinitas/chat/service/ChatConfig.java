c93f7c080f71c44ec84e20ca41ae81e37888a605
Andrey Zhelnin <andjel@gmail.com>
2017-10-20 15:34:58 +0300

    test external value

--- chat_service/src/main/java/de/affinitas/chat/service/ChatConfig.java
+++ chat_service/src/main/java/de/affinitas/chat/service/ChatConfig.java
@@ -2,14 +2,15 @@ package de.affinitas.chat.service;
 
 public class ChatConfig {
 
-    private String mqttRootTopic;
+    private String mqttRootTopic;  
+    @Value("${mqttBrokerUrl}")
     private String mqttBrokerUrl;
     private String mqttUser;
     private String mqttPassword;
 
     public ChatConfig() {
         mqttRootTopic = "affinitas_chat/one_to_one";
-        mqttBrokerUrl = "tcp://localhost:61613";
+        //mqttBrokerUrl = "tcp://localhost:61613";
         mqttUser = "admin";
         mqttPassword = "password";
     }
